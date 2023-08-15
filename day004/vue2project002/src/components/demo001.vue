57cc7ad3949e991d8c04f738fa58344dc417dd21
junw <wo1261931780@gmail.com>
2023-08-15 08:58:29 +0800

    1

---
+++ day004/vue2project002/src/components/demo001.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: junw wo1261931780@gmail.com
+ * @Date: 2023-08-13 20:31:42
+ * @LastEditors: junw wo1261931780@gmail.com
+ * @LastEditTime: 2023-08-13 20:45:36
+ * @FilePath: \vue2project002\src\components\demo001.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  name: "Demo001",
+};
+</script>
+
+<style scoped>
+/* 添加scoped表示 */
+/* 当前的style只在当前文件生效 */
+/* 否则会作用到全局 */
+/* 
+  1.style中的样式 默认是作用到全局的
+  2.加上scoped可以让样式变成局部样式
+
+  组件都应该有独立的样式，推荐加scoped（原理）
+  -----------------------------------------------------
+  scoped原理：
+  1.给当前组件模板的所有元素，都会添加上一个自定义属性
+  data-v-hash值
+  data-v-5f6a9d56  用于区分开不通的组件
+  2.css选择器后面，被自动处理，添加上了属性选择器
+  div[data-v-5f6a9d56]
+*/
+div{
+  border: 3px solid blue;
+  margin: 30px;
+}
+</style>
